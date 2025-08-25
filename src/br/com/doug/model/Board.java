374d2e3a30ced96ce1d13ae5c83de495ecd170c1
ddouglss <ddouglss1999@gmail.com>
2025-08-25 20:32:56 -0300

    adicionando as validações na classe Board

--- src/br/com/doug/model/Board.java
+++ src/br/com/doug/model/Board.java
@@ -1,6 +1,12 @@
 package br.com.doug.model;
 
+import java.util.Collection;
 import java.util.List;
+
+import static br.com.doug.model.GameStatusEnum.*;
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
 
 public class Board {
 
@@ -14,5 +20,49 @@ public class Board {
         return space;
     }
 
+    public GameStatusEnum getStatus(){
+        if(space.stream().flatMap(Collection::stream).noneMatch(s -> !s.isFixed() && nonNull(s.getActual()))){
+            return NON_STARTED;
+        }
+
+        return space.stream().flatMap(Collection::stream)
+                .anyMatch(s -> isNull(s.getActual())) ?
+                INCOMPLETE : COMPLETE;
+    }
+
+    public boolean hasError(){
+        if(getStatus() == NON_STARTED) {
+            return false;
+        }
+
+        return space.stream().flatMap(Collection::stream)
+                .anyMatch(s -> nonNull(s.getActual()) && !s.getActual().equals(s.getExpected()));
+    }
+
+    public boolean changeValue(final int col, final int row, final int value){
+        var spaces = space.get(col).get(row);
+        if (spaces.isFixed()){
+            return false;
+        }
+        spaces.setActual(value);
+        return true;
+    }
+
+    public boolean clearValue(final int col, final int row){
+        var spaces = space.get(col).get(row);
+        if (spaces.isFixed()){
+            return false;
+        }
+        spaces.clearSpace();
+        return true;
+    }
+
+    public void reset(){
+        space.forEach(c -> c.forEach(Space::clearSpace));
+    }
+
+    public boolean gameIsFinished(){
+        return !hasError() && getStatus().equals(COMPLETE);
+    }
 
 }
