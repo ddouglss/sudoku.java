ecced5f7f597d661de0f61dc7fd181115747a4b0
ddouglss <ddouglss1999@gmail.com>
2025-08-26 00:42:18 -0300

    interface do jogo completo pronto para deploy

---
+++ src/br/com/doug/service/EventEnum.java
@@ -0,0 +1,7 @@
+package br.com.doug.service;
+
+public enum EventEnum {
+
+    CLEAR_SPACE
+
+}
---
+++ src/br/com/doug/service/EventListener.java
@@ -0,0 +1,7 @@
+package br.com.doug.service;
+
+public interface EventListener {
+
+    void update(final EventEnum eventType);
+
+}
